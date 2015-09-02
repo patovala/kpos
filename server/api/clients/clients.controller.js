44ff4d44554317cbb87881cbaa5c06a0342e127c
Jimmy <jfernandojb@gmail.com>
2015-09-02 16:13:42 -0500

    test find one clients

--- server/api/clients/clients.controller.js
+++ server/api/clients/clients.controller.js
@@ -28,14 +28,19 @@ exports.findAll = function(req, res) {
 
 //Get clients
 exports.findbyQuery = function(req, res) {
-  MongoClient.connect(url, function(err, db) {
-    console.log("Connected correctly to server for find filtered");
-    var clients = db.collection('clients');
     var str = req.params.query;
-    var val = str.replace('%20', ' ');
-    clients.find({"name": val }).toArray(function(err, docs){
-      res.json(docs);
-      db.close();
-    });
-  });
+    var val = str.replace('%20', ' ').trim();    
+    var patt = /[a-zA-Z0-9_  ]{3,}/g;
+    if(patt.test(val)){
+      console.log("cadena valida"); 
+      MongoClient.connect(url, function(err, db) {
+        console.log("Connected correctly to server for find filtered");
+        var clients = db.collection('clients');        
+        clients.find(query).toArray(function(err, docs){        
+          res.json(docs);
+          db.close();
+        });    
+      });
+    }else
+      console.log("cadena invalida");          
 };
