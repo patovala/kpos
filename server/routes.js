d258670d65ce52b7657bc777a4af132c712273a0
Janina Tatiana Herrera Riofrio <jherrera@ioet.com>
2015-09-03 17:48:03 -0500

    Add query byId

--- server/routes.js
+++ server/routes.js
@@ -12,7 +12,7 @@ var clients = require('./api/clients/clients.controller');
   // Insert routes below
   app.get('/api/clients', clients.findAll);
   app.get('/api/clients/:query?', clients.findbyQuery);
-  app.get('/api/products', products.findAll);
+  app.get('/api/products', products.findAllOrById);
   app.get('/api/products/:query?', products.findbyQuery);
   app.use('/api/things', require('./api/thing'));
   // All undefined asset or api routes should return a 404
