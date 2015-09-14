f516a068ab1a1f1feeef57f0157438f5a3196b8d
Jimmy <jfernandojb@gmail.com>
2015-09-14 14:20:57 -0500

    changes in view html og panel client

--- client/app/popupField/popupField.directive.js
+++ client/app/popupField/popupField.directive.js
@@ -1,11 +1,23 @@
 'use strict';
 
+function panelPopup() {
+  var directive = {
+    templateUrl: 'app/panelPopup/panelField.html',
+    restrict: 'E',
+    link: function () {},
+    controllerAs: 'pp',
+    controller: panelPopupCtrl
+  };
+  return directive;
+  function panelFieldCtrl($scope, $resource, cartService) {
+    var pp = this;
+    pp.qty="";
+
+    function updateQty(idProduct,qty){
+      cartService.updateItemQty(idProduct,qty);  
+      cartService.resetDiscounts();
+      cartService.getDiscountsForCart('byclient');    
+    }
+   }
 angular.module('kposApp')
-  .directive('popupField', function () {
-    return {
-      templateUrl: 'app/popupField/popupField.html',
-      restrict: 'EA',
-      link: function () {
-      }
-    };
-  });
+ .directive('panelField', panelField);
