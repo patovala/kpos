74d85b9a73f12434c320a8c71a82309ef102b470
Patricio Valarezo <patovala@pupilabox.net.ec>
2015-09-18 11:41:53 -0500

    some comments for cleanup

--- client/app/checkoutPanel/checkoutPanel.directive.spec.js
+++ client/app/checkoutPanel/checkoutPanel.directive.spec.js
@@ -12,10 +12,28 @@ describe('Directive: checkoutPanel', function () {
     scope = $rootScope.$new();
   }));
 
+  /*
+   * TODO: volar este
+   * */
   it('should make hidden element visible', inject(function ($compile) {
     element = angular.element('<checkout-panel></checkout-panel>');
     element = $compile(element)(scope);
     scope.$apply();
     expect(element.text()).toBe('this is the checkoutPanel directive');
   }));
+
+  /*
+   * TODO:should get the cart when loaded
+   * */
+
+  /*
+   * When the user enters the ammount tendered, the directive should
+   * resolve the change
+   * */
+
+  /*
+   * Should call backend when checkout button is pressed, the call
+   * should get the cart
+   * */
+
 });
