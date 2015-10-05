f36c06ed7f90ff2f703ea2ea8d939b26a2906751
Cris <cris@cris-Satellite-P745.(none)>
2015-10-05 10:28:39 -0500

    create of service authenticationService

---
+++ client/app/authenticationService/authenticationService.service.js
@@ -0,0 +1,53 @@
+'use strict';
+
+angular.module('kposApp')
+  .service('authenticationService', function ($resource, $cookies, $cookieStore, $location) {
+    var msg='', logid;
+
+    return {
+      logIn: logIn,
+      log0ut: logOut,
+      getMsg: getMsg,
+      getLogId: getLogId,
+      checkStatus: checkStatus
+    };
+
+    function logIn(user, password){
+      var r = $resource('api/authentication/logged');
+      var promise = r.save({user: user, password: password}, function(data){
+        logid = data.logid;
+        if(logid){
+            $cookies.logid = logid;
+            $location.path('/home');
+            msg = 'good login';
+        }else{
+            msg = 'Credenciales inválidas';
+            $location.path('/login');
+        }
+      });
+      return promise;
+    }
+
+    function logOut (){
+        $cookieStore.remove('logid');
+        $location.path('/login');
+    }
+
+    function checkStatus(){
+        if(typeof($cookies.logid) === 'undefined'){
+            $location.path('/login');
+        }else{
+            logid = $cookies.logid;
+            $location.path('/home');
+        }
+    }
+
+    function getMsg(){
+        console.log('debug:' + msg); return msg;
+    }
+
+    function getLogId (){
+        return logid;
+    }
+
+  });
---
+++ client/app/authenticationService/authenticationService.service.spec.js
@@ -0,0 +1,40 @@
+'use strict';
+
+describe('Service: authenticationService', function () {
+
+  // load the service's module
+  beforeEach(module('kposApp'));
+
+  // instantiate service
+  var authenticationService, $httpBackend;
+  beforeEach(inject(function (_authenticationService_, _$httpBackend_) {
+    authenticationService = _authenticationService_;
+    $httpBackend = _$httpBackend_;
+  }));
+
+  afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('#logIn evaluates if the credentials are incorrect', function() {
+    $httpBackend.expectPOST('api/authentication/logged', {user:'cris', password:'guncay'}).respond({result:'error'});
+    authenticationService.logIn('cris', 'guncay');
+    $httpBackend.flush();
+    expect(authenticationService.getMsg()).toEqual('Credenciales inválidas');
+  });
+
+  it('#logIn evaluates the value of id the user', function() {
+    $httpBackend.expectPOST('api/authentication/logged', {user:'cris', password:'guncay'}).respond({logid:'0105220347'});
+    authenticationService.logIn('cris', 'guncay');
+    $httpBackend.flush();
+    expect(authenticationService.getLogId()).toEqual('0105220347');
+  });
+
+  it('#checkStatus evaluates the state user', function() {
+    $httpBackend.expectPOST('api/authentication/logged', {user:'cris', password:'guncay'}).respond({logid:'undefined'});
+    authenticationService.logIn('cris', 'guncay');
+    $httpBackend.flush();
+    expect(authenticationService.getLogId()).toEqual('undefined');
+  });
+});
